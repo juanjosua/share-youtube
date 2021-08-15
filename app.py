d9d21d3c0ff0b72155c2d715737d4881c9a103a9
juanjosua <juan_josua@apps.ipb.ac.id>
2021-08-16 01:24:14 +0700

    display thumbnail from youtube video

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def home():
-    return render_template('index.html', greetings="Welcome to my website!")
+def index():
+    return render_template('index.html')
+
+
+@app.route('/share', methods=['POST'])
+def share():
+    video_code = request.form['youtube_link'].partition("v=")[2]
+    thumbnail = 'https://i.ytimg.com/vi_webp/{}/sddefault.webp'.format(
+        video_code)
+
+    return render_template('display.html', thumbnail=thumbnail)
 
 
 if __name__ == '__main__':
